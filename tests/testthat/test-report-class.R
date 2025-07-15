42b74ee8940e614f9fe1b1d0343d4f09a7af22a2
jewell <jewell@bafg.de>
2025-07-15 13:44:55 +0200

    cleaned up the tests

--- tests/testthat/test-report-class.R
+++ tests/testthat/test-report-class.R
@@ -70,7 +70,17 @@ test_that("False positive in multiple indicies can be deleted", {
 })
 
 test_that("Reintegration runs without error", {
-  test <- loadReport(F, test_path("fixtures", "testReportReintegration.report"))
+  # Prepare test files for reintegration
+  test <- Report$new()
+  test$changeSettings("pol", "pos")
+  test$addDB(F, test_path("fixtures", "CSL_olmesartan-d6.db"))
+  test$addIS(F, test_path("fixtures", "IS_table_pos.csv"))
+  test$addRawFiles(F, c(
+    test_path("fixtures", "mzXML-test-files", "RH_pos_20230812.mzXML"),
+    test_path("fixtures", "mzXML-test-files", "RH_pos_20230813.mzXML"),
+    test_path("fixtures", "mzXML-test-files", "RH_pos_20220602_no_peaks.mzXML")
+  ))
+  test$process_all()
   test$reIntegrate()
   expect_equal(nrow(test$integRes), 2)
 })
